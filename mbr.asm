378dd6e111159f276ea29757370029869f73b836
Tiago <tmsilva11@gmail.com>
2019-09-23 00:01:18 -0300

    Final release

--- mbr.asm
+++ mbr.asm
@@ -1,9 +1,9 @@
-	;; mbr.asm - A simple x86 bootloader example.
-	;;
-	;; In worship to the seven hacker gods and for the honor 
-	;; of source code realm, we hereby humbly offer our sacred 
-	;; "Hello World" sacrifice. May our code remain bugless.
-
+;; Copyright (c) <2019> - <Fernando Gorgulho Fayet> <fer.fayet@usp.br> - <Tiago Marino Silva> <tmsilva11@usp.br>
+;;
+;; This is free software and distributed under GNU GPL vr.3. Please 
+;; refer to the companion file LICENSING or to the online documentation
+;; at https://www.gnu.org/licenses/gpl-3.0.txt for further information.
+	
 	
 	org 0x7c00		; Our load address
 
@@ -11,64 +11,149 @@
 
 	mov bx, 0		; May be 0 because org directive.
 
-loop:				; Write a 0x0-terminated ascii string
-	mov al, [here + bx]	
-	int 0x10
-	cmp al, 0x0
-	je end
-	add bx, 0x1		
-	jmp loop
+	jmp main
 
-end:				; Jump forever (same as jmp end)
-	jmp $
+	;; STRINGS ESTÁTICAS
+	start: db "Quiz++", 0xd, 0xa, 0x0
 
-here:				; C-like NULL terminated string
+	firQ: db "How many Bytes does a Bootloader have?", 0xd, 0xa, 0x0
+	firA: db "1)100  2)1024  3)8  4)512", 0xd, 0xa, 0x0
 
-	db 'Hello world!', 0xd, 0xa, 0x0
-	
+	secQ: db "What was Einstens' dog name?", 0xd, 0xa, 0x0
+	secA: db "1)Rex  2)It's relative  3)Doggo  4)Liam", 0xd, 0xa, 0x0
+
+	thiQ: db "Who invented the computer?", 0xd, 0xa, 0x0
+	thiA: db "1)James  2)Ballen Touring  3)Alan Turing  4)Truing Allen", 0xd, 0xa, 0x0
+
+	fouQ: db "Which is the best OS?", 0xd, 0xa, 0x0
+	fouA: db "1)Linux  2)Windows  3)Mac OS  4)Brick OS", 0xd, 0xa, 0x0
+
+	wrongTxt: db "Wrong", 0xd, 0xa, 0x0
+	rightTxt: db "Right", 0xd, 0xa, 0x0
+
+main:
+	mov bx, start
+    call printString    ; Prints intro string
+
+	mov bx, firQ		;; Print 1a pergunta
+    call printString
+	mov bx, firA
+    call printString
+
+	call scanNumber		;; le 1a resposta
+	mov dx, 4
+	call checkAnwser
+
+	mov bx, secQ		;; Print 2a pergunta
+    call printString
+	mov bx, secA
+    call printString
+
+	call scanNumber		;; le 2a resposta
+	mov dx, 2
+	call checkAnwser
+
+	mov bx, thiQ		;; Print 3a pergunta
+    call printString
+	mov bx, thiA
+    call printString
+
+	call scanNumber		;; le 3a resposta
+	mov dx, 3
+	call checkAnwser
+
+	mov bx, fouQ		;; Print 4a pergunta
+    call printString
+	mov bx, fouA
+    call printString
+
+	call scanNumber		;; le 4a resposta
+	mov dx, 4
+	call checkAnwser
+
+	jmp endAll
+
+right:
+	mov bx, rightTxt
+    call printString    ;; Print resposta correta
+
+	ret
+
+wrong:
+	mov bx, wrongTxt
+    call printString    ;; Print resposta errada
+
+	ret
+
+checkAnwser:
+
+	cmp bx, dx			;; checa a resposta dada (bx) com a correta (dx)
+	je right
+	jmp wrong
+
+;; PRINT STRING  ------------------------------------------------------------
+;; --------------------------------------------------------------------------
+;; imprime na tela a string localizada no endereço de bx
+printString:
+    push bx
+    push cx
+
+    jmp nullLoop
+    ; bx is the address register
+    ; cx is the char register
+
+nullLoop:
+    mov cl, [bx]
+    cmp cl, 0x0
+    je endPrintString
+
+    call printChar
+    add bx, 0x1
+    jmp nullLoop
+
+endPrintString:
+    pop cx
+    pop bx
+    ret
+
+
+printChar:
+    push ax
+
+    ; Sets arguments for BIOS interrupt
+    mov ah, 0x0e
+    mov al, cl
+    int 0x10
+
+    pop ax
+    ret
+
+;; SCAN NUMBER  ----------------------------------------------------------------
+;; -----------------------------------------------------------------------------
+;; recebe um char do teclado, converte em int e coloca em bx
+scanNumber:
+    push ax
+    push cx
+    mov bx, 0
+
+    mov ah, 0x0
+    int 0x16    ; Le char
+
+    movzx dx, al ;; converte em int 
+    sub dx, '0' ; 
+
+    imul bx, 0xA
+
+    add bx, dx  
+
+    pop cx
+    pop ax
+
+    ret
+
+;; TERMINATE BOOT SEQUENCE ----------------------------------------------------
+;; ---------------------------------------------------------------------------
+endAll:
+
 	times 510 - ($-$$) db 0	; Pad with zeros
 	dw 0xaa55		; Boot signature
-
-		
-	;; Notes (remove these comments for your code).
-	;; 
-	;; This assembly source code is written for x86 architecture in intel 
-	;; syntax and NASM  assembler dialect. It's mean to be compiled with 
-	;; NASM assembler.
-	;; 
-	;; A label (such as 'loop:') is interpreted by the prepossessesor as
-	;; the offset to (byte count at) the next command (jmp instruction, 
-	;; in this example).
-	;; 
-	;; The directive org 0x7c00 intructs the compiler to automatically
-	;; add the load address to the offset when necessary. Therefore,
-	;; 'mov al, label' virtually becomes 'mov al, label + 0x7c00'. 
-	;; 
-	;; BIOS interruption 'int 0x10' causes the execution flow to jump to 
-	;; the interruption vector table area, where there is a pre-loaded BIOS
-	;; routine capable of outputing characters to the video controller.
-	;; This interruption handler routine reads the byte at the 8-bit
-	;; register and send to the video controller. The video operation
-	;; mode (e.g. ascii character) is controlled by register ah.
-	;; After completing the operation, execution flow is returned to
-	;; the next line after 'int' instruction.
-	;;
-	;; The argument of jmp and je instructions, here, is a relative offset. 
-	;;
-	;; The line db causes the ouput of 1-byte patters at the current
-	;; position in the generated machine code. For instance, the string
-	;; 'Hello World' followed by newline and return ascii codes is inserted
-	;; in the given location.
-	;; 
-	;; The directive 'times X Y' produces a sequence of X repetitions of
-	;; of Y. The type specification 'db' means that Y is a byte (8 bits).
-	;; If it were dw, it would mean 'word', i.e. 16 bits.
-	;;
-	;; Symbol $  denotes the address of (byte count at) the current line.
-	;; Symbol $$ denotes the address of (byte count at) the start of current
-	;; section (in present case, we have only one section). Therefore, the
-	;; value ($-$$) is the current address minus the address of the
-	;; program start. We need 510 minus this amount of zeros.
-	;;
-	;; The line dw causes the output of the 2-byte pattern for the boot
-	;; signature at the current position (positions 511 and 512).
